2943e86671ca219b348475f770032c5adb25b3a6
Borhan Uddin <borhaninfos@gmail.com>
2022-03-06 17:49:12 +0600

    header fix

--- src/components/pages/header/Navigation.js
+++ src/components/pages/header/Navigation.js
@@ -1,41 +1,51 @@
 import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 export default function Navigation() {
   return (
-    <div className="row main-head">
-      <div className="col-lg-4 logo">
-        <img src="images/logo.png" alt="" />
-      </div>
-      <div className="col-lg-8 menu">
-        <nav className="main-menu">
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="3">Story</a>
-            </li>
-            <li>
-              <a href="">Services</a>
-            </li>
-            <li>
-              <a href="">Design</a>
-            </li>
-            <li>
-              <a href="">Work</a>
-            </li>
-            <li>
-              <a href="">Testimonials</a>
-            </li>
-            <li>
-              <a href=""> Clients</a>
-            </li>
-            <li>
-              <a href="">Blog</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="main-head">
+      <Navbar color="warning" container dark expand="md" fixed="top" light>
+        <NavbarBrand href="/">
+          <img src="images/logo.png" alt="" />
+        </NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck() {}} />
+        <Collapse navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Story</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Design</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Work</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Testimonials</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Clients</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Blog</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
--- src/components/pages/myclients/MyClients.js
+++ src/components/pages/myclients/MyClients.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function MyClients() {
   return (
     <section id="my-clients">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 my-clients">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 my-clients">
             <img src="images/my-clients.png" alt="" />
           </div>
         </div>
